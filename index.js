e3192c7fc19ed39237e0a540cfdff04816248ba1
zeeshujunaid <zeeshan.junaid.developer@gmail.com>
2024-05-01 12:22:08 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+                //     FIRST QUESTION
+                //  Print all even numbers from 0 – 10
+//    for (let i = 0; i <= 10; i++) {  if (i % 2 === 0) { console.log(i); }}
+
+                     //  SECOND QUESTION                         
+                     // Create a length converter function                 
+//    var feet = document.getElementById("feet");
+//  var inch = document.getElementById("inch");
+
+//  feet.addEventListener('input', function(){
+//      let f = this.value;
+//      let i = f * 12;
+//      inch.value = i;
+//  })
+
+//  inch.addEventListener('input', function(){
+//     let i = this.value;
+//     let f = i / 12;
+//     if (!Number.isInteger(f)) {
+//         f=f.toFixed(2);
+//     }
+//     feet.value = f;
+// })
+
+//   third QUESTION   
+// Create a function that reverses an array
+// var a = ["zeeshan", "hassan", "ahmed","shebaz"];
+// console.log(a , "a");
+// a.sort();
+// console.log(a , "a");
+// a.reverse();
+// console.log(a , "a");
+
+
+//  FOURTH QUESTION
+// Sort an array from lowest to highest
+
+// var a = [7, 10, 3, 4, 5, 6, 7, 8, 9,1, 2, ];
+
+// a.sort(function(a, b) {
+//     return b - a;
+// });
+// console.log(a);
+
+//  FIFTH QUESTION
+//  Return a Boolean if a number is divisible by 10
+// function boleannumber(num) {
+//     return num % 10 === 0 ? true  : false;    
+// }
+// console.log(boleannumber(25));
